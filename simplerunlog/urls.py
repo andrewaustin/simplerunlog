ea430786c0b2c1fff0ee7cc30c92124e1a5d68d1
Andrew Austin <andrewaclt@gmail.com>
2012-09-12 17:36:06 -0400

    Remove unused import

--- simplerunlog/urls.py
+++ simplerunlog/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns
 from django.views.generic import ListView
-from django.views.generic.simple import direct_to_template
 
 from apps.runlog.models import Run
 
