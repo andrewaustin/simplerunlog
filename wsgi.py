fc005217c012dcc7249b7029d2250ddadb4a22cd
Andrew Austin <andrewaclt@gmail.com>
2012-09-14 15:12:30 -0400

    Change simplerunlog to runfanatic

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for simplerunlog project.
+WSGI config for runfanatic project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplerunlog.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "runfanatic.settings.dev")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
