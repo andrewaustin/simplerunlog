05140620ff4b028de2cf3f6c16d7682f9d4e5198
Andrew Austin <andrewaclt@gmail.com>
2012-09-14 22:06:33 -0400

    After logging out redirect to /

--- runfanatic/urls.py
+++ runfanatic/urls.py
@@ -17,6 +17,6 @@ urlpatterns = patterns('apps.runlog.views',
 
 urlpatterns += patterns('',
     (r'^login/$', 'django.contrib.auth.views.login'),
-    (r'^logout/$', 'django.contrib.auth.views.logout'),
+    (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )
 
